95138639ea2689ff98611a7dcf9a8f4d4b2e363a
WangZ <wangzhen951120@outlook.com>
2020-09-07 23:29:39 +0800

    修改 dist 配置

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,7 @@ function resolve(dir) {
 }
 
 module.exports = {
+    publicPath: "/web-answer/dist/",
     // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
     productionSourceMap: false,
     devServer: {
